485d03281e761cf32aa4c5d2f15540de3bcc6f26
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-03-07 20:04:23 +0530

    change port

--- filen/run.sh
+++ filen/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node filen.js webdav --data-dir /addon_config/  --w-https --w-user $WEBDAV_USERNAME --w-password $WEBDAV_PASSWORD
+node filen.js webdav --data-dir /addon_config/ --w-https --w-user $WEBDAV_USERNAME --w-password $WEBDAV_PASSWORD
