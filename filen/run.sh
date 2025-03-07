e5077f5fbaa7ded7e63552e407609b9973a1d284
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-03-07 21:39:54 +0530

    testing

--- filen/run.sh
+++ filen/run.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-WEBDAV_USERNAME=$(bashio::config 'WEBDAV_USERNAME')
-WEBDAV_PASSWORD=$(bashio::config 'WEBDAV_PASSWORD')
-FILEN_EMAIL=$(bashio::config 'FILEN_EMAIL')
+#!/usr/bin/with-contenv bashio
+
+export WEBDAV_USERNAME=$(bashio::config 'WEBDAV_USERNAME')
+export WEBDAV_PASSWORD=$(bashio::config 'WEBDAV_PASSWORD')
+export FILEN_EMAIL=$(bashio::config 'FILEN_EMAIL')
+export FILEN_PASSWORD=$(bashio::config 'FILEN_PASSWORD')
+export FILEN_2FA_CODE=$(bashio::config 'FILEN_2FA_CODE')
+
 bashio::log.info "credentials --- '${FILEN_EMAIL}:${WEBDAV_USERNAME}:${WEBDAV_PASSWORD}'..."
 node filen.js webdav --email $(bashio::config 'FILEN_EMAIL') --password $(bashio::config 'FILEN_PASSWORD') --two-factor-code $(bashio::config 'FILEN_2FA_CODE') --data-dir /addon_config/ --w-https --w-user $(bashio::config 'WEBDAV_USERNAME') --w-password $(bashio::config 'WEBDAV_PASSWORD')
