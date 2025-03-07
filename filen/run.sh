5aba058fb0599476afaf78f6ff149e7e714814dd
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-03-07 22:59:07 +0530

    testing

--- filen/run.sh
+++ filen/run.sh
@@ -1,10 +1,18 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
+set -e
 
-export WEBDAV_USERNAME=$(bashio::config 'WEBDAV_USERNAME')
-export WEBDAV_PASSWORD=$(bashio::config 'WEBDAV_PASSWORD')
-export FILEN_EMAIL=$(bashio::config 'FILEN_EMAIL')
-export FILEN_PASSWORD=$(bashio::config 'FILEN_PASSWORD')
-export FILEN_2FA_CODE=$(bashio::config 'FILEN_2FA_CODE')
+CONFIG_PATH=/data/options.json
 
-bashio::log.info "credentials --- '${FILEN_EMAIL}:${WEBDAV_USERNAME}:${WEBDAV_PASSWORD}'..."
+WEBDAV_USERNAME=$(jq --raw-output '.WEBDAV_USERNAME // empty' $CONFIG_PATH)
+WEBDAV_PASSWORD=$(jq --raw-output '.WEBDAV_PASSWORD // empty' $CONFIG_PATH)
+FILEN_EMAIL=$(jq --raw-output '.FILEN_EMAIL // empty' $CONFIG_PATH)
+FILEN_PASSWORD=$(jq --raw-output '.FILEN_PASSWORD // empty' $CONFIG_PATH)
+FILEN_2FA_CODE=$(jq --raw-output '.FILEN_2FA_CODE // empty' $CONFIG_PATH)
+
+echo "${WEBDAV_USERNAME}"
+echo "${WEBDAV_PASSWORD}"
+echo "${FILEN_EMAIL}"
+echo "${FILEN_PASSWORD}"
+echo "${FILEN_2FA_CODE}"
+
 node filen.js webdav --email $(bashio::config 'FILEN_EMAIL') --password $(bashio::config 'FILEN_PASSWORD') --two-factor-code $(bashio::config 'FILEN_2FA_CODE') --data-dir /addon_config/ --w-https --w-user $(bashio::config 'WEBDAV_USERNAME') --w-password $(bashio::config 'WEBDAV_PASSWORD')
