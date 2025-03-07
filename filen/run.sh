a50b0130a43e32d300fda7b80059be6fcd1deb92
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-03-07 19:55:05 +0530

    filen addon changes

--- filen/run.sh
+++ filen/run.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-#exec filen webdav --w-user $WEBDAV_USERNAME --w-password $WEBDAV_PASSWORD
-echo $FILEN_EMAIL
-echo $FILEN_PASSWORD
-echo $FILEN_2FA_CODE
-echo $WEBDAV_USERNAME
-echo $WEBDAV_PASSWORD
+node filen.js webdav --data-dir /addon_config/  --w-https --w-user $WEBDAV_USERNAME --w-password $WEBDAV_PASSWORD
